b4a401716c8cc9357a001e6a1a3a0c66536f48b7
Milarqui <59261420+Milarqui@users.noreply.github.com>
2020-01-08 17:03:52 +0100

    Create NVL-PHP-FUNC-3.php

---
+++ NVL-PHP-FUNC-3.php
@@ -0,0 +1,22 @@
+function vocales($Frase) {
+  $vals = ["a","e","i","o","u"];
+  $esta = [False,False,False,False,False];
+  $frase = strtolower($Frase);
+  $l = strlen($frase);
+  for($i = 0; $i < 5; $i++)
+  {
+    if(strpos($frase,$vals[$i]) !== False)
+    {
+      $esta[$i] = True;
+    }
+  }
+  if(in_array(False, $esta)){
+      return "NO CONTIENE TODAS LAS VOCALES";
+  }
+  else {
+     return "LA PALABRA CONTIENE LAS 5 VOCALES";
+  }
+}
+
+$P = "Hoy es un nuevo dia";
+echo vocales($P);
