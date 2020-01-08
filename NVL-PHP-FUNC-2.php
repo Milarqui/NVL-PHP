aacb855312c99a8a6ec84f6f7528b4bfbe0dd3a0
Milarqui <59261420+Milarqui@users.noreply.github.com>
2020-01-08 16:44:06 +0100

    Create NVL-PHP-FUNC-2.php

---
+++ NVL-PHP-FUNC-2.php
@@ -0,0 +1,15 @@
+function cuentaLetras($palabra, $letra)
+{
+  $n = 0;
+  $l = strlen($palabra);
+  $low = strtolower($letra);
+  $upp = strtoupper($letra);
+  for($i = 0; $i < $l; $i++)
+  {
+    if($palabra[$i] == $low or $palabra[$i] == $upp)
+    {
+      $n++;
+    }
+  }
+  return $n;
+}
