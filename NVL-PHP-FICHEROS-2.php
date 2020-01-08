0c9314bba86fc47da1ea636d6cf1dcfd792a93c7
Milarqui <59261420+Milarqui@users.noreply.github.com>
2020-01-08 18:06:36 +0100

    Update NVL-PHP-FICHEROS-2.php

--- NVL-PHP-FICHEROS-2.php
+++ NVL-PHP-FICHEROS-2.php
@@ -1,14 +1,14 @@
 function reemplazar($read,$write,$word1,$word2)
 {
-  $fIn = fopen($read,"r");
-  $fOut = fopen($write,"w");
-  while(($lIn = fgets($fIn)) !== false)
+  $fIn = fopen($read,"r"); \\Abre el archivo a leer
+  $fOut = fopen($write,"w"); \\Abre o crea el archivo donde se escribe
+  while(($lIn = fgets($fIn)) !== false) \\Lee cada línea del archivo de entrada
   {
-    $lOut = str_replace($word1,$word2,$lIn);
-    fwrite($fOut,$lOut);
+    $lOut = str_replace($word1,$word2,$lIn); \\Para cada línea, reemplaza la palabra a buscar por la palabra a reemplazar
+    fwrite($fOut,$lOut); \\Escribe la nueva línea en el archivo de salida
   }
-  fclose($fIn);
-  fclose($fOut);
+  fclose($fIn); \\Cierra el archivo de entrada
+  fclose($fOut); \\Cierra el archivo de salida
 }
 
 $routeIn = "https://gist.githubusercontent.com/jsdario/6d6c69398cb0c73111e49f1218960f79/raw/8d4fc4548d437e2a7203a5aeeace5477f598827d/el_quijote.txt";
